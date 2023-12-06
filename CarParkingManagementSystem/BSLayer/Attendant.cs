3bbddc2c8eef203e098d8c5acea69402fd009b1d
Suzukisakae <lethanhvinhspk@gmail.com>
2023-12-06 21:35:29 +0700

    up một số class

---
+++ CarParkingManagementSystem/BSLayer/Attendant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarParkingManagementSystem.DBLayer;
+
+namespace CarParkingManagementSystem.BSLayer
+{
+    public class Attendant
+    {
+        Connector db = new Connector();
+        public Attendant() { }
+    }
+}
